45e655b0af5844cc9a69b9dfe452a1927271c46a
Ottavia Epifania <41794883+OttaviaE@users.noreply.github.com>
2025-02-13 22:56:26 +0100

    update sis

--- sis-2025.R
+++ sis-2025.R
@@ -99,7 +99,13 @@ resFrank = list()
 resLeon = list()
 # qui dentro devo mettere solo la TIF calcolata sui 50 item con stanchezza
 resAllitems = list()
-
+time = data.frame(iteration = 1:100, 
+                  start_all = numeric(100), 
+                  end_all = numeric(100), 
+                  start_frank = numeric(100), 
+                  end_frank = numeric(100), 
+                  start_leon = numeric(100), 
+                  end_leon = numeric(100))
 for (i in 1:100){
   set.seed(1312+i)
   q[[i]] = data.frame(b = runif(n_item, -3, 3), 
@@ -113,19 +119,110 @@ for (i in 1:100){
   q_tired[[i]] = q[[i]]
   q_tired[[i]]$e =  exp(-0.01*(0:(nrow(q_tired[[i]])-1)))
   temp_tired_iifs = item_info(q_tired[[i]], theta = theta)
+  time[i, "start_all"] = Sys.time()
   resAllitems[[i]] = data.frame(theta = theta, 
                                 tif_all = rowMeans(temp_tired_iifs))
+  time[i, "end_all"] = Sys.time()
   # applico frank e leon 
+  time[i, "start_frank"] = Sys.time()
   resFrank[[i]] = frank(q_tired[[i]], tif[[i]])
+  time[i, "end_frank"] = Sys.time()
+  time[i, "start_leon"] = Sys.time()
   resLeon[[i]] = leon(q_tired[[i]], tif[[i]])
-  print(cat(paste("Replication", i)))
+  time[i, "end_leon"] = Sys.time()
+  print(paste("Replication", i))
 }
 # cose che posso fare
 # guardare la differenza simmetrica tra gl item selezionati dai due algfortimi 
 # calcolare la distanza media dalla TIF target (forse questa è la cosa migliore)
 colnames(resFrank[[1]]$iif_stf)[order(colnames(resFrank[[1]]$iif_stf))]
+for (i in 1:20) {
+  plot(theta, tif[[i]]$mean_tif, type = "l", main = i)
+  lines(theta, resAllitems[[i]]$tif_all, 
+        type = "l", col = "red")
+  lines(theta, rowMeans(resFrank[[i]]$iif_stf),
+        type = "l", col = "green")
+  lines(theta, rowMeans(resLeon[[i]]$iif_stf), 
+        type = "l", col = "royalblue")
+}
 
-plot(theta, tif[[i]]$mean_tif, type = "l")
-lines(theta, resAllitems[[i]]$tif_all, type = "l", col = "red")
-lines(theta, rowMeans(resFrank[[i]]$iif_stf), type = "l", col = "green")
-lines(theta, rowMeans(resLeon[[i]]$iif_stf), type = "l", col = "royalblue")
+
+# per ogni iterazione, devo prendere la tif target in tif[[i]]$mean_tif
+# la tif con tutti gli item stanchi
+# la tif di frank e quella di leon 
+# per loro due potrebbe valer la pena di tenere anche gli insiemi q_leon e q_frank
+
+temp = NULL
+myresults = NULL
+res_tot = NULL
+for (i in 1:length(resAllitems)) {
+  temp = data.frame(theta = theta, 
+                    target = tif[[i]]$mean_tif, 
+                    all = resAllitems[[i]]$tif_all, 
+                    frank = rowMeans(resFrank[[i]]$iif_stf), 
+                    leon =  rowMeans(resLeon[[i]]$iif_stf), 
+                    n_frank = ncol(resFrank[[i]]$iif_stf), 
+                    n_leon =  ncol(resLeon[[i]]$iif_stf), 
+                    q_frank = resFrank[[i]]$q_frank, 
+                    q_leon = resLeon[[i]]$q_leon,
+                    iteration = i)
+  myresults = rbind(myresults, temp)
+  tempAll = data.frame(theta = theta, 
+                       target = tif[[i]]$mean_tif, 
+                       type = "all", 
+                       value = resAllitems[[i]]$tif_all, 
+                       iteration = i)
+  tempFrank = data.frame(theta = theta, 
+                       target = tif[[i]]$mean_tif, 
+                       type = "frank", 
+                       value =rowMeans(resFrank[[i]]$iif_stf), 
+                       iteration = i)
+  tempLeon = data.frame(theta = theta, 
+                         target = tif[[i]]$mean_tif, 
+                         type = "leon", 
+                         value =rowMeans(resLeon[[i]]$iif_stf), 
+                         iteration = i)
+  res_tot = rbind(res_tot, tempAll, tempFrank, tempLeon)
+}
+res_tot$difference = abs(res_tot$target - res_tot$value)
+library(wesanderson)
+wes_palette("GrandBudapest2", n=3)
+mycolor = c(wes_palette("Chevalier1", n = 2), wes_palette("Chevalier1", n = 4)[4])
+ggplot(res_tot, 
+       aes(x = type, y = difference, color = type)) + 
+  geom_boxplot(linewidth=1.2) + 
+  theme_light() + ylab(expression(paste("|TIF", "* - ",TIF[x], "|"))) + 
+  scale_x_discrete(labels = c("All items", "Frank", "Léon")) +
+  theme(axis.text = element_text(size = 26), 
+        axis.title.y = element_text(size = 28), 
+        axis.title.x = element_blank(), 
+        legend.position = "none") + 
+  scale_color_manual(values= mycolor)
+
+ggsave("C:/Users/Ottavia/Documents/GitHub/frank-and-friends/sis2025/LaTex+Package/styles/img/box-plot-alogirthms.pdf", 
+       device = "pdf", width = 8.5, height = 14, units = "in")
+myRes = aggregate(difference ~ type + iteration, data = res_tot, mean)
+quantile(myRes$difference)
+mean_difference = aggregate(difference ~ type , data = res_tot, mean)
+colnames(mean_difference)[2] = "meandiff"
+myRes = merge(myRes, mean_difference)
+ggplot(myRes, 
+       aes(x = reorder(factor(iteration), difference), 
+           y = difference, color = type, shape = type)) + 
+  geom_point(size = 2)  + 
+  theme_light() + xlab("Replication") + ylab(expression(paste("|TIF", "* - ",TIF[x], "|"))) + 
+  scale_x_discrete(labels = c("All items", "Frank", "Léon")) +
+  theme(axis.text = element_text(size = 26), 
+        axis.text.x = element_blank(),
+        axis.title = element_text(size = 28),
+        legend.position = "none") + 
+  scale_color_manual(values= mycolor) + 
+  geom_hline(aes(yintercept  = meandiff, color = type), 
+             linewidth=2, linetype = 2)
+
+res_tot %>%  
+  group_by(type) %>%  
+  summarise(mean = round(mean(difference),2), 
+            sd = round(sd(difference),2), 
+            min = round(min(difference), 8), 
+            max = round(max(difference), 2))
